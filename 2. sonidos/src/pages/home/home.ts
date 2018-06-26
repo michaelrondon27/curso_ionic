351bca4dd34db6426909235a95ccdd70de222326
mrondon72 <mrondon72@gmail.com>
2018-06-26 11:15:08 -0400

    mostrando un listado de todos los animales *ngFor

--- 2. sonidos/src/pages/home/home.ts
+++ 2. sonidos/src/pages/home/home.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ANIMALES } from '../../data/data.animales';
+
+import { Animal } from '../../interfaces/animal.interface';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales: Animal[] = [];
+
+  constructor() {
+
+    this.animales = ANIMALES.slice(0);
 
   }
 

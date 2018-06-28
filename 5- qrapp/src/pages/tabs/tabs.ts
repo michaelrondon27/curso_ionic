c47efa45aa0412e7e99f13695a7e7d0d6745a319
mrondon72 <mrondon72@gmail.com>
2018-06-28 10:16:24 -0400

    creacion de los tabs y un modelo de datos

--- 5- qrapp/src/pages/tabs/tabs.ts
+++ 5- qrapp/src/pages/tabs/tabs.ts
@@ -1,25 +1,17 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the TabsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { HomePage, GuardadosPage } from '../index.paginas';
 
-@IonicPage()
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html',
 })
 export class TabsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  tab1: any = HomePage;
+  tab2: any = GuardadosPage;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TabsPage');
+  constructor() {
   }
 
 }

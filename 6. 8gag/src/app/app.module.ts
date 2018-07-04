b533cddf28ae2b5332237276d9f5321c512b6e06
mrondon72 <mrondon72@gmail.com>
2018-07-04 15:12:34 -0400

    actualizando - conexion a firebase

--- 6. 8gag/src/app/app.module.ts
+++ 6. 8gag/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
+import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { SubirPage } from '../pages/subir/subir';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB-TYbrxiXJxPTpPm6rP7-AoXmSrywWAMw",
+  authDomain: "gag-f22d0.firebaseapp.com",
+  databaseURL: "https://gag-f22d0.firebaseio.com",
+  projectId: "gag-f22d0",
+  storageBucket: "gag-f22d0.appspot.com",
+  messagingSenderId: "906295192989"
+};
 
 @NgModule({
   declarations: [
@@ -17,7 +29,10 @@ import { SubirPage } from '../pages/subir/subir';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,6 +43,7 @@ import { SubirPage } from '../pages/subir/subir';
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })

a35498233267c3dc0585de1ef41d3fca40791592
mrondon72 <mrondon72@gmail.com>
2018-07-10 13:22:39 -0400

    demostracion del ciclo de vida de una pagina

--- 7. miscelaneos/src/pages/pagina2/pagina2.ts
+++ 7. miscelaneos/src/pages/pagina2/pagina2.ts
@@ -18,4 +18,57 @@ export class Pagina2Page {
 
   }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad');
+  }
+
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter');
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+  }
+
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave');
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave');
+  }
+
+  ionViewWillUnload() {
+    console.log('ionViewWillUnload');
+  }
+
+  ionViewCanEnter() {
+    console.log('ionViewCanEnter');
+
+    let numero = Math.round( Math.random() * 10 );
+    console.log(numero)
+
+    if ( numero >= 3 ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  ionViewCanLeave() {
+    console.log('ionViewCanLeave');
+
+    console.log("Espera 2 segundos para salir");
+
+    let promesa = new Promise( (resolve, reject) => {
+
+      setTimeout( () => {
+        resolve(true);
+      }, 2000);
+
+    });
+
+    return promesa;
+  }
+
 }

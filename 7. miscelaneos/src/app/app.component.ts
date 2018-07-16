791101ef9a4fc148aeb3bcd53e0b5916631a6e3c
mrondon72 <mrondon72@gmail.com>
2018-07-16 14:57:18 -0400

    leer los ajustes para poder determinar que pagina mostrar al inicio

--- 7. miscelaneos/src/app/app.component.ts
+++ 7. miscelaneos/src/app/app.component.ts
@@ -5,15 +5,32 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 
+import { AjustesProvider } from '../providers/ajustes/ajustes';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  // rootPage:any = HomePage;
-  rootPage:any = "IntroduccionPage";
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  rootPage: any;
+
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    private _ajustes: AjustesProvider
+  ) {
+
     platform.ready().then(() => {
+
+      this._ajustes.cargar_storage();
+
+      if ( this._ajustes.ajustes.mostrar_tutorial ) {
+        this.rootPage = "IntroduccionPage";
+      } else {
+        this.rootPage = HomePage;
+      }
+
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
